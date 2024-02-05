080bda308b506a303fabc4bae7202f62b3972ca0
spandana-test <mspandana1995@gmail.com>
2024-02-05 18:01:35 +0530

    changes

--- cypress/pages/contactuspage.js
+++ cypress/pages/contactuspage.js
@@ -28,30 +28,7 @@ class contactusPage{
     }
     
     
-//    submitEnquiry(){
-    
-    
-//    this.elements.firstName().type("Spandana");
-    
-    
-//    this.elements.lastName().type("Mantripragada")
-    
-    
-//    this.elements.email().type("testemail@gmail.com")
-    
-    
-//    this.elements.mobileNumber().type("9000890987")
-    
-    
-//    this.elements.selectDropsdown().select("LinkedIn")
 
-//    this.elements.message().type("Test message");
-
-//    this.elements.submitButton().click();
-
-    
-    
-//    }
     
     
     }
